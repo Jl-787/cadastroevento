806898a182e99f99a276d8a9265244efd3ba9e3a
joaolucas23 <joaolucas.araujof@gmail.com>
2022-01-23 22:21:15 -0300

    cadastro evento

---
+++ cadastroevento.js
@@ -0,0 +1,31 @@
+let dataEvento = '25'
+
+if(dataEvento >= 25) {
+    console.log("data do evento correta, podemos cadastrar") 
+} else {
+    console.log("data do evento inválida, não podemos casdastrar")
+} 
+
+let idadeParticipante = '18'
+
+if(idadeParticipante >= 18) {
+    console.log("idade do participante aceitável,podemos cadastrar")
+} else {
+    console.log("idade do participante inválida para cadastro")
+}
+
+let palestrantes = '10'
+
+if(palestrantes >= 10) {
+    console.log("idade do palestrante aceitável,podemos cadastrar")
+} else {
+    console.log("idade do palestrante inválida para cadastro")
+}
+
+let participantes = '100'
+
+if(participantes <= 100) {
+    console.log ("número de participantes válido para cadastro")
+} else {
+    console.log ("número de participantes excedeu, inválido para cadastro")
+}
